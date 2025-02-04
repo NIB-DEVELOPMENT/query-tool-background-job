2323cacbd447168728ff05b0cf10bc75d8ee61d5
Lionel Smith <LSmith@nib-bahamas.com>
2025-02-04 17:05:52 -0500

    refactor: update schema name to use dynamic Oracle username

--- src/queries/query_model.py
+++ src/queries/query_model.py
@@ -1,6 +1,7 @@
 from sqlalchemy import *
 from src import engine, base
+from config import OracleDB
 
 class QueryTable(base):
     __tablename__ = 'query_table'
-    __table_args__ = ({'schema': 'nib_query_tool_ls_1', 'autoload_with': engine, "extend_existing" : True})
+    __table_args__ = ({'schema': OracleDB().userName, 'autoload_with': engine, "extend_existing" : True})
