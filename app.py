c936339117265ec6965e0c9b9735c943f77413f5
Trxtxn <142917052+Trxtxn@users.noreply.github.com>
2025-07-14 10:45:21 -0400

    increased prefetch count to 100

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
         ch.basic_ack(delivery_tag=method.delivery_tag)
         
         
-    channel.basic_qos(prefetch_count=1)
+    channel.basic_qos(prefetch_count=100)
     channel.basic_consume(
         queue=Queue.QUERY_REPORT_QUEUE,
         on_message_callback=callback,
